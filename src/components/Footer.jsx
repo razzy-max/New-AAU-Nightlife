2461927fafd56ebfa056ab47dc99883b69d83ab9
Irabor Erasmus <eraskid6@gmail.com>
2025-12-22 07:41:26 +0100

    Update copyright year to 2026

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 AAU Nightlife. All rights reserved.</p>
+      <p>&copy; 2026 AAU Nightlife. All rights reserved.</p>
       <p>Contact: info@aaunightlife.com</p>
     </footer>
   );
